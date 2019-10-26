ad24eac13661e4312cd2df03a6994820994a16d8
evanmasterson <evanm95@hotmail.com>
2019-10-26 15:40:54 +0000

    remove unused devise helper

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,5 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    # if !user_signed_in?
-    #   flash[:notice] = "Sign in to access"
-    # end
   end
 end
