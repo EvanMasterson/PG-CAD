71133b6c3d365b10387830965d3915479fe490a3
EvanMasterson <evanm95@hotmail.com>
2019-11-03 13:26:47 +0000

    add admin trait to users, add user confirmation, show email in console

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -117,10 +117,10 @@ Devise.setup do |config|
   # config.pepper = '3bd6b80677cf2a3f491d70d1fb4e428391f33d211dfbcbac343a3d5c45b12c71bb0c692727d95a4168fffa757f9edf3d69a915359eea74018dbb2388f2a5e294'
 
   # Send a notification to the original email when the user's email is changed.
-  # config.send_email_changed_notification = false
+  config.send_email_changed_notification = true
 
   # Send a notification email when the user's password is changed.
-  # config.send_password_change_notification = false
+  config.send_password_change_notification = true
 
   # ==> Configuration for :confirmable
   # A period that the user is allowed to access the website even without
@@ -139,13 +139,13 @@ Devise.setup do |config|
   # their account can't be confirmed with the token any more.
   # Default is nil, meaning there is no restriction on how long a user can take
   # before confirming their account.
-  # config.confirm_within = 3.days
+  config.confirm_within = 3.days
 
   # If true, requires any email changes to be confirmed (exactly the same way as
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
   # db field (see migrations). Until confirmed, new email is stored in
   # unconfirmed_email column, and copied to email column on successful confirmation.
-  config.reconfirmable = true
+  config.reconfirmable = false
 
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [:email]
