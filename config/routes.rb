50d8fe672dada0fe902e233ab1ee3b2b43c356b9
bbordas <bogi.t.bordas@gmail.com>
2019-11-04 20:19:34 +0000

    uploaded_files added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :uploaded_files
   devise_for :users, path: 'user'
   
   root :to =>'pages#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191103120305) do
+ActiveRecord::Schema.define(version: 20191104200551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "uploaded_files", force: :cascade do |t|
+    t.string   "name"         default: "",    null: false
+    t.integer  "size"         default: 0,    null: false
+    t.string   "description"
+    t.string   "unique_url"   null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -21,10 +30,6 @@ ActiveRecord::Schema.define(version: 20191103120305) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
