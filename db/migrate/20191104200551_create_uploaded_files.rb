cb17b99a4660ceabd062bda68ddddff387d4bc2e
EvanMasterson <evanm95@hotmail.com>
2019-11-05 18:00:51 +0000

    fix migration/schema

--- db/migrate/20191104200551_create_uploaded_files.rb
+++ db/migrate/20191104200551_create_uploaded_files.rb
@@ -1,10 +1,10 @@
 class CreateUploadedFiles < ActiveRecord::Migration[5.0]
   def change
     create_table :uploaded_files do |t|
-      t.string :name
-      t.integer :size
+      t.string :name, null: false, default: ""
+      t.integer :size, null: false, default: 0
       t.string :description
-      t.string :unique_url
+      t.string :unique_url, null: false, default: ""
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,12 @@ ActiveRecord::Schema.define(version: 20191104200551) do
   enable_extension "plpgsql"
 
   create_table "uploaded_files", force: :cascade do |t|
-    t.string   "name"         default: "",    null: false
-    t.integer  "size"         default: 0,    null: false
+    t.string   "name",        default: "", null: false
+    t.integer  "size",        default: 0,  null: false
     t.string   "description"
-    t.string   "unique_url"   null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "unique_url",  default: "", null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,6 +30,10 @@ ActiveRecord::Schema.define(version: 20191104200551) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
