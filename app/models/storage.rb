ebfa6578b02b7b0abc7fe963fc9260d9a61f601a
bbordas <bogi.t.bordas@gmail.com>
2019-11-12 16:48:00 +0000

    many to many between storages and files

--- app/models/storage.rb
+++ app/models/storage.rb
@@ -1,3 +1,4 @@
 class Storage < ApplicationRecord
   has_and_belongs_to_many :users
+  has_and_belongs_to_many :uploaded_files
 end
---
+++ db/migrate/20191112164045_storgage_files.rb
@@ -0,0 +1,4 @@
+class StorgageFiles < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
