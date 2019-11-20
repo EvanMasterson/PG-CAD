551af34338a948465c4d21c35b3e52de5295dd43
EvanMasterson <evanm95@hotmail.com>
2019-11-20 21:02:14 +0000

    setting up amazon ses smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,12 +26,18 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Devise config
+  # Amazon SES config
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.smtp_settings = {
+    :address => ENV["SES_SMTP_ADDRESS"],
+    :port => ENV["SES_SMTP_PORT"],
+    :user_name => ENV["SES_SMTP_USERNAME"],
+    :password => ENV["SES_SMTP_PASSWORD"],
+    :authentication => :login,
+    :enable_starttls_auto => true
+  }
 
   config.action_mailer.perform_caching = false
 
